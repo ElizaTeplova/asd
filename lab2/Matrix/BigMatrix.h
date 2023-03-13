dde212cb6ec429eeb76ce61d9b9258d0d033d868
Nikita Rakcheev <107801291+ElizaTeplova@users.noreply.github.com>
2023-03-14 01:03:24 +0300

    Lab2 almost done. I rewrite inverse method and it works correctly

--- lab2/Matrix/BigMatrix.h
+++ lab2/Matrix/BigMatrix.h
@@ -24,34 +24,41 @@ private:
             std::cerr << "currentMatrixSize == 0: return 0!" << std::endl;
             return 0;
         }
-        supportMatrix[column] = false;
+        supportMatrix[column] = currentMatrixSize == sizeMatrix - 1;
         if (currentMatrixSize > 2) {
 
-            for (i = 0; i < sizeMatrix; i++) {
-                if (i == column) {
+            for (i = 0; i < sizeMatrix - 1; i++) {
+                if (i == column && currentMatrixSize != sizeMatrix - 1) {
                     continue;
                 }
-                det += matrix[row + 1][i] * findMinor(matrix, supportMatrix, row + 1, i, currentMatrixSize - 1);
-                for (i = 0; i < sizeMatrix; i++) {
-                    supportMatrix[column] = true;
-                }
+                supportMatrix[i] = false;
+                std::cout << "---------------------------------------------------------------" << std::endl;
+                std::cout << "---------------------------------------------------------------" << std::endl;
+                std::cout << "matrix[" << row << "][" << i << "]: " << matrix[row + 1][i] << std::endl;
+                det += matrix[row][i] * findMinor(matrix, supportMatrix, row + 1, i, currentMatrixSize - 1);
             }
+            supportMatrix[column] = true;
         } else {
+//            supportMatrix[column] = true;
             double numMatrix[2][2] = {0};
             int iIndex = 0, jIndex = 0, j2Index = 0;
-            for (i = sizeMatrix - 2; i < sizeMatrix; i++) {
-                for (j = 0; j < sizeMatrix; j++) {
+            for (i = sizeMatrix - 2 - 1; i < sizeMatrix - 1; i++) {
+                for (j = 0; j < sizeMatrix - 1; j++) {
                     if (supportMatrix[j] == true) {
-                        numMatrix[iIndex][jIndex] = matrix[iIndex][j2Index];
+//                        numMatrix[iIndex][jIndex] = matrix[iIndex][j2Index];
+                        numMatrix[iIndex][jIndex] = matrix[i][j];
                         jIndex++;
-                        j2Index++;
+//                        j2Index++;
                     }
                 }
-                j2Index = 0;
+//                j2Index = 0;
                 jIndex = 0;
                 iIndex++;
             }
-            std::cout << "numMatrix" << std::endl;
+            print1DArray<bool>(
+                    "findMinor(double **matrix, bool *supportMatrix, int row, int column, int currentMatrixSize): supportMatrix",
+                    supportMatrix, sizeMatrix - 1);
+            std::cout << "numMatrix: " << std::endl;
             for (int i = 0; i < 2; i++) {
                 for (int j = 0; j < 2; j++) {
                     std::cout << std::fixed;
@@ -62,9 +69,7 @@ private:
                 }
                 std::cout << std::endl;
             }
-            print1DArray<bool>(
-                    "findMinor(double **matrix, bool *supportMatrix, int row, int column, int currentMatrixSize): supportMatrix",
-                    supportMatrix, sizeMatrix);
+
             supportMatrix[column] = true;
 //            print2DArray<double>("findMinor(double **matrix, bool *supportMatrix, int row, int column, int currentMatrixSize): numMatrix", (double **)numMatrix, sizeMatrix);
 //            for (i = 0; i < sizeMatrix; i++) {
@@ -84,7 +89,7 @@ private:
         for (i = 0; i < sizeMatrix - 1; i++) {
             adjSupport[i] = new double[sizeMatrix - 1];
         }
-        print2DArray<T>("getAdjSupport(double **adj, int *row, int *column): adj", adj, sizeMatrix);
+//        print2DArray<T>("getAdjSupport(double **adj, int *row, int *column): adj", adj, sizeMatrix);
 //        static double adjSupport[sizeMatrix - 1][sizeMatrix - 1];
         for (i = 0; i < sizeMatrix; i++) {
             if (i == *row) {
@@ -100,10 +105,23 @@ private:
             jIndex = 0;
             iIndex++;
         }
-
+        std::cout << "---------------------------------------------------------------" << std::endl;
         print2DArray<double>("getAdjSupport(double **adj, int *row, int *column): adjSupport: ", adjSupport,
                              sizeMatrix - 1);
         return adjSupport;
+    }
+
+    void multipleMatrix(double **matrix, int row, double k) {
+        std::cout << "k: " << k << std::endl;
+        for (int j = 0; j < sizeMatrix; j++) {
+            matrix[row][j] *= k;
+        }
+    }
+
+    void subtractMatrix(double **matrix, int row, double k) {
+        for (int j = 0; j < sizeMatrix; j++) {
+            matrix[row][j] -= k;
+        }
     }
 
 public:
@@ -111,14 +129,18 @@ public:
     void inverse() {
         int i = 0, j = 0, factor = 0;
         double det = 0;
-        double** result = new double*[sizeMatrix];
-        bool *supportMatrix = new bool[sizeMatrix];
+        double **result = new double *[sizeMatrix];
+        bool *supportMatrix = new bool[sizeMatrix - 1];
         double **adj = new double *[sizeMatrix];
         double **adjSupport;
 
+        for (int i = 0; i < sizeMatrix - 1; i++) {
+            supportMatrix[i] = true;
+        }
+
         for (i = 0; i < sizeMatrix; i++) {
             result[i] = new double[sizeMatrix];
-            supportMatrix[i] = true;
+//            supportMatrix[i] = true;
             adj[i] = new double[sizeMatrix];
 //            supportMatrix[i] = true;
             for (j = 0; j < sizeMatrix; j++) {
@@ -126,11 +148,12 @@ public:
             }
         }
         i = 0;
-        for (j = 0; j < sizeMatrix; j++) {
-            adjSupport = getAdjSupport(a, &i, &j);
-            factor = (0 + j) % 2 == 0 ? 1 : -1;
-            det += this->a[0][j] * factor * findMinor(adjSupport, supportMatrix, 0 + 1, j, sizeMatrix - 1);
-        }
+//        for (j = 0; j < sizeMatrix; j++) {
+//            adjSupport = getAdjSupport(a, &i, &j);
+//            factor = (0 + j) % 2 == 0 ? 1 : -1;
+//            det += this->a[0][j] * factor * findMinor(adjSupport, supportMatrix, 0 + 1, j, sizeMatrix - 1);
+//        }
+        det = 1;
         std::cout << "det: " << det << std::endl;
         if (det == 0) {
             std::cerr << "det == 0. Operation prohibited" << std::endl;
@@ -141,8 +164,9 @@ public:
                 adjSupport = getAdjSupport(a, &i, &j);
                 factor = (i + j) % 2 == 0 ? 1 : -1;
                 adj[i][j] = factor * findMinor(adjSupport, supportMatrix, i + 1, j, sizeMatrix - 1);
-                adj[i][j] /= det;
+//                adj[i][j] /= det;
                 result[j][i] = adj[i][j];
+
             }
         }
         print2DArray<double>("inverse(): result: ", result, sizeMatrix);
@@ -188,6 +212,104 @@ public:
         return stream;
     }
 
+    void secondMethodInverse() {
+        double **rightMatrix = new double *[sizeMatrix];
+        double **cloneMatrix = new double *[sizeMatrix];
+        double factor = 0;
+        int i = 0, j = 0, k = 0;
+
+        for (i = 0; i < sizeMatrix; i++) {
+            rightMatrix[i] = new double[sizeMatrix];
+            cloneMatrix[i] = new double[sizeMatrix];
+            for (j = 0; j < sizeMatrix; j++) {
+                rightMatrix[i][j] = i == j ? 1 : 0;
+                cloneMatrix[i][j] = a[i][j];
+            }
+        }
+//        print2DArray("cloneMatrix: ", cloneMatrix, sizeMatrix);
+        double denomenator = 1 / cloneMatrix[0][0];
+        multipleMatrix(cloneMatrix, 0, denomenator);
+        multipleMatrix(rightMatrix, 0, denomenator);
+        cloneMatrix[0][0] = 1;
+//        print2DArray("rightMatrix: ", rightMatrix, sizeMatrix);
+        printTwoArrays("", cloneMatrix, rightMatrix, sizeMatrix);
+
+        for (j = 0; j < sizeMatrix - 1; j++) {
+            for (i = j + 1; i < sizeMatrix; i++) {
+                if (cloneMatrix[j][j] == 0 || cloneMatrix[i][j] == 0) {
+                    continue;
+                }
+
+                factor = cloneMatrix[i][j] / cloneMatrix[j][j];
+                std::cout << "i: " << i << " | j: " << j << " | factor: " << factor << std::endl;
+//                actions with clone matrix:
+                multipleMatrix(cloneMatrix, i, 1 / factor);
+
+//                actions with single matrix:
+                multipleMatrix(rightMatrix, i, 1 / factor);
+                for (k = 0; k < sizeMatrix; k++) {
+                    cloneMatrix[i][k] -= cloneMatrix[j][k];
+                    rightMatrix[i][k] -= rightMatrix[j][k];
+                }
+                std::cout << "--------------------------------------" << std::endl;
+                std::cout << "--------------------------------------" << std::endl;
+//                print2DArray("cloneMatrix: ", cloneMatrix, sizeMatrix);
+//                print2DArray("rightMatrix: ", rightMatrix, sizeMatrix);
+                printTwoArrays("", cloneMatrix, rightMatrix, sizeMatrix);
+            }
+
+            multipleMatrix(cloneMatrix, j + 1, 1 / cloneMatrix[j + 1][j + 1]);
+            multipleMatrix(rightMatrix, j + 1, 1 / cloneMatrix[j + 1][j + 1]);
+
+//            print2DArray("cloneMatrix: ", cloneMatrix, sizeMatrix);
+//            print2DArray("rightMatrix: ", rightMatrix, sizeMatrix);
+            printTwoArrays("", cloneMatrix, rightMatrix, sizeMatrix);
+            std::cout << "--------------------------------------" << std::endl;
+            std::cout << "--------------------------------------" << std::endl;
+        }
+
+
+        for (i = 1; i < sizeMatrix; i++) {
+            for (j = 0; j < i; j++) {
+                cloneMatrix[i][j] = 0;
+            }
+        }
+        std::cout << "--------------------------------------" << std::endl;
+        std::cout << "--------------------------------------" << std::endl;
+        printTwoArrays("", cloneMatrix, rightMatrix, sizeMatrix);
+//        print2DArray("cloneMatrix: ", cloneMatrix, sizeMatrix);
+//        print2DArray("rightMatrix: ", rightMatrix, sizeMatrix);
+        std::cout << "Above main diagonal: " << std::endl;
+
+        for (j = 1; j < sizeMatrix; j++) {
+            for (i = 0; i < j; i++) {
+                factor = cloneMatrix[i][j];
+
+                for (k = 0; k < sizeMatrix; k++) {
+                    cloneMatrix[i][k] -= cloneMatrix[j][k] * factor;
+                    rightMatrix[i][k] -= rightMatrix[j][k] * factor;
+                }
+                std::cout << "j: " << j << std::endl;
+                printTwoArrays("", cloneMatrix, rightMatrix, sizeMatrix);
+//                print2DArray("cloneMatrix: ", cloneMatrix, sizeMatrix);
+//                print2DArray("rightMatrix: ", rightMatrix, sizeMatrix);
+                std::cout << "--------------------------------------" << std::endl;
+                std::cout << "--------------------------------------" << std::endl;
+            }
+        }
+
+
+        print2DArray("right matrix: ", rightMatrix, sizeMatrix);
+
+        for (i = 0; i < sizeMatrix; i++) {
+            delete[] rightMatrix[i];
+            delete[] cloneMatrix[i];
+        }
+
+        delete[] rightMatrix;
+        delete[] cloneMatrix;
+    }
+
     template<typename K>
     void print2DArray(const std::string &messege, K **array2D, int arrSize) {
         std::cout << messege << std::endl;
@@ -195,8 +317,8 @@ public:
             for (int j = 0; j < arrSize; j++) {
                 std::cout << std::fixed;
                 std::cout.setf(std::ios::right);
-                std::cout.width(5);
-                std::cout.precision(3);
+                std::cout.width(10);
+                std::cout.precision(5);
                 std::cout << array2D[i][j] << " ";
             }
             std::cout << std::endl;
@@ -216,6 +338,30 @@ public:
 
         }
         std::cout << std::endl;
+    }
+
+    template<typename K>
+    void printTwoArrays(const std::string &messege, K **arrayLhs, K **arrayRhs, int arrSize) {
+        std::cout << messege << std::endl;
+        for (int i = 0; i < arrSize; i++) {
+            for (int j = 0; j < arrSize; j++) {
+                std::cout << std::fixed;
+                std::cout.setf(std::ios::right);
+                std::cout.width(10);
+                std::cout.precision(5);
+                std::cout << arrayLhs[i][j] << " ";
+            }
+
+            std::cout << "|";
+            for (int j = 0; j < arrSize; j++) {
+                std::cout << std::fixed;
+                std::cout.setf(std::ios::right);
+                std::cout.width(10);
+                std::cout.precision(5);
+                std::cout << arrayRhs[i][j] << " ";
+            }
+            std::cout << std::endl;
+        }
     }
 };
 
--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <ctime>
+#include <chrono>
 #include "Matrix/Matrix.h"
 #include "Matrix/BigMatrix.h"
 
 int main() {
     int i, j, k;
     int arrayInt[3][3] = {
-            {1, 2, 3},
-            {4, 5, 6},
-            {7, 2, 9}
+            {1, 2, 1},
+            {0, 1, 0},
+            {0, 2, 2}
     };
     int **pArrayInt = new int *[3];
     for (k = 0; k < 3; k++) {
@@ -20,7 +21,17 @@ int main() {
         }
     }
     BigMatrix<int> bigMatrix(pArrayInt, 3);
-    bigMatrix.inverse();
+    auto start = std::chrono::system_clock::now();
+    bigMatrix.secondMethodInverse();
+    auto  end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end-start;
+    std::cout << "duration method: " << elapsed_seconds.count() << "s" << std::endl;
+
+    for (j = 0; j < 3; j++) {
+        delete[] pArrayInt[i];
+    }
+    delete[] pArrayInt;
+
 //    int testSize;
 //    int** intRandMatrix = nullptr;
 //    std::srand(std::time(nullptr)); // use current time as seed for random generator
@@ -34,14 +45,19 @@ int main() {
 //        intRandMatrix[i] = new int[testSize];
 //
 //        for (j = 0; j < testSize; j++) {
-//            intRandMatrix[i][j] = std::rand() % 10;
+//            intRandMatrix[i][j] = std::rand() % 100;
 //            std::cout << intRandMatrix[i][j] << " ";
 //        }
 //        std::cout << std::endl;
 //    }
-
+//
 //    BigMatrix<int> bigMatrixInt(intRandMatrix, testSize);
 //    std::cout << bigMatrixInt;
+//    auto start = std::chrono::system_clock::now();
+//    bigMatrixInt.secondMethodInverse();
+//    auto end = std::chrono::system_clock::now();
+//    std::chrono::duration<double> elapsed_seconds = end-start;
+//    std::cout << "duration method: " << elapsed_seconds.count() << "s" << std::endl;
 
 //    for (i = 0; i < testSize; i++) {
 //        delete[] intRandMatrix[i];
